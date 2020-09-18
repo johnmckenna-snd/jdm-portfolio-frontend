51605d85c4585a7b8f23f2d86a4211937592afd8
johnmckenna-snd <63067728+johnmckenna-snd@users.noreply.github.com>
2020-09-18 13:30:33 -0700

    updated nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
+import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
+
+const NavWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	align-items: baseline;
+	justify-content: center;
+	flex-wrap: wrap;
+	margin-left: 10px;
+`;
+
+const NavH1 = styled.h1`
+	color: ${colors.grey3};
+	font-weight: ${fonts.bold.weight};
+	font-size: 26px;
+	margin-right: 24px;
+	margin-top: 46px;
+`;
+
+const NavP = styled.p`
+color: ${colors.grey4};
+font-weight: ${fonts.regular.weight};
+font-size: 10px;
+margin-left: 46px;
+`;
 
 const Nav = () => {
 	return (
-		<h1>john  mckenna</h1>
+		<NavWrapper>
+			<NavH1>john</NavH1>
+			<NavH1>mckenna</NavH1>
+			<NavP>sound designer + web designer</NavP>
+		</NavWrapper>
 	);
 };
 
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,6 +3,7 @@ export default {
 	grey1: 'rgba(230, 230, 230, 1)',
 	grey2: 'rgba(188, 188, 188, 1)',
 	grey3: 'rgba(109, 110, 112, 1)',
+	grey4: 'rgba(35, 31, 32, 1)',
 	green1: 'rgba(142, 255 ,78, 1)',
 	yellow1: 'rgba(232, 277, 59, 1)',
 	purple1: 'rgba(176, 131, 207, 1)',
