b6f020ccac5635afe2b8a2f643ad2d1aa5737a89
johnmckenna-snd <63067728+johnmckenna-snd@users.noreply.github.com>
2020-09-18 13:43:02 -0700

    added 404 placeholder

---
+++ src/components/404/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const E404 = () => {
+	return (
+		<h1>404 - crap</h1>
+	);
+};
+
+export default E404;
--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useRoutes } from 'hookrouter';
+import styled from 'styled-components';
 import routes from './routes';
+import E404 from '../404/404';
+
+const RouterWrapper = styled.div`
+	margin: auto;
+`;
 
 const Router = () => {
 	const routeResult = useRoutes(routes);
 	return (
-		routeResult
+		<RouterWrapper>
+			{routeResult || <E404 />}
+		</RouterWrapper>
 	);
 };
 
