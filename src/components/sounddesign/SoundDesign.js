9ed6d3fb0a1c2c4d1578d90a43625f0aac46b1f0
johnmckenna-snd <63067728+johnmckenna-snd@users.noreply.github.com>
2020-09-22 16:52:38 -0700

    done for the day. will build detail page factory tomorrow

--- src/components/sounddesign/SoundDesign.js
+++ src/components/sounddesign/SoundDesign.js
@@ -6,34 +6,60 @@ const SoundDesignWrapper = styled.div`
 	display: flex;
 	flex-direction: row;
 	flex-wrap: wrap;
-	width: 750px;
-	height: 300px;
-	margin: 160px auto 0px auto;
+	width: 1080px;
+	height: 100%;
+	margin: 20px auto 0px auto;
 `;
 
-const SoundDesignCard = styled.button`
+const SoundDesignHeaderWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	width: 100%;
+	height: 46px;
+	margin: 10px 10px 20px 10px;
+	background: linear-gradient(90deg, ${colors.purple1_70} 0%, ${colors.blue1_70} 100%);
+	border-radius: 10px;
+`;
+
+const SoundDesignH1 = styled.h1`
+	font-size: 32px;
+	margin: 3px 0px 0px 20px;
+	padding: 0px;
+	color: ${colors.grey3};
+`;
+
+const SoundDesignCardWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	margin: 10px 0 0 0;
+`;
+
+const SoundDesignCard = styled.div`
 	display: flex;
 	flex-direction: column;
-	width: 350px;
-	height: 130px;
-	background: linear-gradient(90deg, ${colors.purple1_70} 0%, ${colors.blue1_70} 100%);
+	width: 520px;
+	height: 320px;
+	background: ${colors.grey2_70};
 	border-radius: 10px;
 	margin: 10px;
 	border: none;
 
-	&:hover {
-		background: ${colors.purple1_70};
-	}
 `;
 
 const SoundDesign = () => {
 	return (
 		<SoundDesignWrapper>
-			<h1>Sound Design</h1>
-			<SoundDesignCard />
-			<SoundDesignCard />
-			<SoundDesignCard />
-			<SoundDesignCard />
+			<SoundDesignHeaderWrapper>
+				<SoundDesignH1>sound design</SoundDesignH1>
+			</SoundDesignHeaderWrapper>
+			<SoundDesignCardWrapper>
+				<SoundDesignCard />
+				<SoundDesignCard />
+				<SoundDesignCard />
+				<SoundDesignCard />
+			</SoundDesignCardWrapper>
 		</SoundDesignWrapper>
 	);
 };
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,6 +3,7 @@ export default {
 	grey1: 'rgba(230, 230, 230, 1)',
 	grey1_40: 'rgba(230, 230, 230, .4)',
 	grey2: 'rgba(188, 188, 188, 1)',
+	grey2_70: 'rgba(188, 188, 188, .7)',
 	grey3: 'rgba(109, 110, 112, 1)',
 	grey4: 'rgba(35, 31, 32, 1)',
 	green1: 'rgba(142, 255 ,78, 1)',
